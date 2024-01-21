1bae63ca2518912b9adeadf2995a30ad3168200a
ChoiSungwook <sungwook0724@lguplus.co.kr>
2024-01-21 23:38:29 +0900

    disable Argo rollout dashboard at helm intall

--- install/argo/argo-rollouts/install.sh
+++ install/argo/argo-rollouts/install.sh
@@ -2,5 +2,5 @@
 
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
-helm upgrade --install argo-rollout argo/argo-rollouts --version 2.31.0 -n argo-rollouts --set "dashboard.enabled=true" --create-namespace
+helm upgrade --install argo-rollout argo/argo-rollouts --version 2.31.0 -n argo-rollouts --set "dashboard.enabled=false" --create-namespace
 kubectl apply -f ./argo-rollouts-dashboard.yaml
